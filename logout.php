0834f9688ca1bee1b3aeadbcdacd473a68da9a20
kalki2001 <74086626+kalki2001@users.noreply.github.com>
2022-05-04 20:55:21 +0530

    Update logout.php

--- logout.php
+++ logout.php
@@ -39,10 +39,7 @@
             </div>
             <footer class="footer">
                <div class="container">
-                <center>
-                   <p>Copyright &copy <a href="https://projectworlds.in">Projectworlds</a> Store. All Rights Reserved.</p>
-                   <p>This website is developed by Yugesh Verma</p>
-               </center>
+                
                </div>
            </footer>
         </div>
